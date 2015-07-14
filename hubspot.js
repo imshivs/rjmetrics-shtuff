48784586ca6e41661ef377c3acf2b3f676d368eb
Shivani Negi <negishivs@gmail.com>
2015-07-13 17:42:17 -0700

    hubspot things

--- hubspot.js
+++ hubspot.js
@@ -1,20 +1,33 @@
+// require('dotenv').load();
+// var hubspotAPI = require('hubspot');
+
+// var token = process.env.HUBSPOT_API_KEY;
+
+// try {
+//     var api = hubspotAPI({
+//       token: token,
+//       version : 'v3'
+//     });
+// } catch (error) {
+//     console.log(error.message); // the options are missing, this function throws an error.
+// }
+
+// api.sources({ user_id: 419727 }, function (error, data) {
+//     if (error)
+//         console.log(error.message);
+//     else
+//         console.log(JSON.stringify(data)); // Do something with your data!
+// });
+
 require('dotenv').load();
-var hubspotAPI = require('hubspot');
 
-var token = process.env.HUBSPOT_API_KEY;
+var Client = require('node-hubspot');
 
-try {
-    var api = hubspotAPI({
-      token: token,
-      version : 'v3'
-    });
-} catch (error) {
-    console.log(error.message); // the options are missing, this function throws an error.
-}
+var client = new Client();
 
-api.sources({ user_id: 30 }, function (error, data) {
-    if (error)
-        console.log(error.message);
-    else
-        console.log(JSON.stringify(data)); // Do something with your data!
+client.useKey(process.env.HUBSPOT_API_KEY);
+client.useToken(process.env.HUBSPOT_API_KEY);
+client.campaigns.get(function(err, res) {
+    if (err) { throw err; }
+    console.log(res);
 });
--- import.js
+++ import.js
@@ -1,28 +1,28 @@
 var rjmetrics = require("rjmetrics");
 var client = new rjmetrics.Client(0, "RJ_METRICS_IMPORT_KEY");
 
-function syncUser(client, user) {
-  return client.pushData(
-    // table named "users"
-    "users",
-    {
-      // user_id is the unique key here, since each user should only
-      // have one record in this table
-      "keys": ["id"],
-      "id": user.id,
-      "email": user.email,
-      "acquisition_source": user.acquisition_source
-    });
-}
+// function syncUser(client, user) {
+//   return client.pushData(
+//     // table named "users"
+//     "users",
+//     {
+//       // user_id is the unique key here, since each user should only
+//       // have one record in this table
+//       "keys": ["id"],
+//       "id": user.id,
+//       "email": user.email,
+//       "acquisition_source": user.acquisition_source
+//     });
+// }
 
-// let's define some fake users
-var users = [
-  {id: 1, email: "joe@schmo.com", acquisition_source: "PPC"},
-  {id: 2, email: "mike@smith.com", acquisition_source: "PPC"},
-  {id: 3, email: "lorem@ipsum.com", acquisition_source: "Referral"},
-  {id: 4, email: "george@vandelay.com", acquisition_source: "Organic"},
-  {id: 5, email: "larry@google.com", acquisition_source: "Organic"},
-];
+// // let's define some fake users
+// var users = [
+//   {id: 1, email: "joe@schmo.com", acquisition_source: "PPC"},
+//   {id: 2, email: "mike@smith.com", acquisition_source: "PPC"},
+//   {id: 3, email: "lorem@ipsum.com", acquisition_source: "Referral"},
+//   {id: 4, email: "george@vandelay.com", acquisition_source: "Organic"},
+//   {id: 5, email: "larry@google.com", acquisition_source: "Organic"},
+// ];
 
 // make sure the client is authenticated before we do anything
 client.authenticate().then( function(data) {
