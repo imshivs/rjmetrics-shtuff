52505af1164fb3aa9ab8b63f7c634f90bda6f7b2
Shivani Negi <negishivs@gmail.com>
2015-07-15 18:10:29 -0700

    import not working

--- import.js
+++ import.js
@@ -1,5 +1,7 @@
+
+require('dotenv').load();
 var rjmetrics = require("rjmetrics");
-var client = new rjmetrics.Client(0, "RJ_METRICS_IMPORT_KEY");
+var client = new rjmetrics.Client(0, process.env.RJ_METRICS_IMPORT_KEY);
 
 // function syncUser(client, user) {
 //   return client.pushData(
@@ -41,46 +43,105 @@ var client = new rjmetrics.Client(0, "RJ_METRICS_IMPORT_KEY");
 //   console.error("Failed to authenticate!");
 // });
 
-var http = require('http');
-var request = require('request');
-var url = require('url');
+// var http = require('http');
+// var request = require('request');
+// var url = require('url');
 
-//single data point
-var data = [{
-  "keys": ["id"],
-  "id": 1,
-  "email": "joe@schmo.com",
-  "status": "pending",
-  "created_at": "2012-08-01 14:22:32"
-},{
-  "keys": ["id"],
-  "id": 2,
-  "email": "anne@schmo.com",
-  "status": "pending",
-  "created_at": "2012-08-03 23:12:30"
-},{
-  "keys": ["id"],
-  "id": 1,
-  "email": "joe@schmo.com",
-  "status": "complete",
-  "created_at": "2012-08-05 04:51:02"
-}]
+// //single data point
+// var data = [{
+//   "keys": ["id"],
+//   "id": 1,
+//   "email": "joe@schmo.com",
+//   "status": "pending",
+//   "created_at": "2012-08-01 14:22:32"
+// },{
+//   "keys": ["id"],
+//   "id": 2,
+//   "email": "anne@schmo.com",
+//   "status": "pending",
+//   "created_at": "2012-08-03 23:12:30"
+// },{
+//   "keys": ["id"],
+//   "id": 1,
+//   "email": "joe@schmo.com",
+//   "status": "complete",
+//   "created_at": "2012-08-05 04:51:02"
+// }]
 
-var buf = new Buffer(data);
-var username = "shivani@npmjs.com",
-    password = "AgeQuodAgis@5",
-    importApi = 'https://connect.rjmetrics.com/v2/client/3986/table/test/data?apikey=',
-    auth = process.env.RJ_METRICS_IMPORT_KEY
+// var buf = new Buffer(data);
+// var username = "shivani@npmjs.com",
+//     password = "AgeQuodAgis@5",
+//     importApi = 'https://connect.rjmetrics.com/v2/client/:cid/table/:table/data?apikey=',
+//     auth = process.env.RJ_METRICS_IMPORT_KEY
 
-request.post(
-    {
-        url : importApi + auth,
-        headers : {
-            "X-RJM-API-Key" : auth,
-            "content-type" : "application/json"
-        }
-    },
-    function (error, response, body) {
-      console.log(body);
-    }
-);
+// request.post(
+//     {
+//         url : importApi + auth,
+//         headers : {
+//             "X-RJM-API-Key" : auth,
+//             "content-type" : "application/json"
+//         }
+//     },
+//     function (error, response, body) {
+//       console.log(body);
+//     }
+// );
+
+// var test_data = {
+//   "keys": ["id"],
+//   "id": 1,
+//   "email": "joe@schmo.com",
+//   "status": "pending",
+//   "created_at": "2012-08-01 14:22:32"
+// }
+
+client.authenticate().then(function(data) {
+  console.log("here");
+  client.pushData(
+    "test_table",
+     { "keys": ["id"],
+      "id": 1,
+      "email": "joe@schmo.com",
+      "status": "pending",
+      "created_at": "2012-08-01 14:22:32"
+     })
+  }).then(function (data) {
+    console.log("saved:", data);
+  }).fail(function(err) {
+    console.log("Error", err, "saving.");
+  })
+
+
+// function syncOrder(client, order) {
+//   return client.pushData(
+//     "orders",
+//     {
+//       "keys": ["id"],
+//       "id": order.id,
+//       "user_id": order.user_id,
+//       "value": order.value,
+//       "sku": order.sku
+//     });
+// }
+
+// var orders = [
+//   {"id": 1, "user_id": 1, "value": 58.40,  "sku": "milky-white-suede-shoes"},
+//   {"id": 2, "user_id": 1, "value": 23.99,  "sku": "red-button-down-fleece"},
+//   {"id": 3, "user_id": 2, "value": 5.00,   "sku": "bottle-o-bubbles"},
+//   {"id": 4, "user_id": 3, "value": 120.01, "sku": "zebra-striped-game-boy"},
+//   {"id": 5, "user_id": 5, "value": 9.90  , "sku": "kitten-mittons"}
+// ];
+
+// client.authenticate().then( function(data) {
+
+//   orders.forEach( function(order) {
+//     syncOrder(client, order).then( function(data) {
+//       console.log("Synced order with id " + order.id);
+//     }, function(error) {
+//       console.error("Failed to sync order with id " + order.id);
+//     })
+//   });
+
+// }).fail(function(err) {
+//   console.error("Failed to authenticate!");
+// });
